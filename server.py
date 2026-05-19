1af6b6d00ec711b811f0dd2b908d40952d73c1e7
Tushar <tusharmks9816@gmail.com>
2026-05-19 09:54:10 +0530

    Fix: threading mode, remove eventlet/gevent

--- server.py
+++ server.py
@@ -6,7 +6,3 @@ sys.path.insert(0, os.path.join(os.path.dirname(os.path.abspath(__file__)), 'bac
 from app import create_app
 
 app = create_app()
-
-if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port, debug=False)
